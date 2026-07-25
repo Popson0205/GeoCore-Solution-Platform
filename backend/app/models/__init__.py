78b1cd2430a46bc30877ef2682f33d0859d7bb1d
Popoola Idris Bamigboye <idrispopoola02@gmail.com>
2026-07-25 16:28:20 +0100

    Delete backend directory

--- backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Import every model here so Base.metadata is aware of all tables
-# when Base.metadata.create_all() runs at startup.
-from backend.app.models.user import User  # noqa: F401
-from backend.app.models.organisation import Organisation, OrganisationMember  # noqa: F401
-from backend.app.models.project import Project  # noqa: F401
